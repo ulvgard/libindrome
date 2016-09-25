bf5df93599ff76e3a24588533a538571f676fab9
Tobias Ulvgard <tobias.ulvgard@dirac.se>
2016-09-25 17:29:49 +0200

    lsvideo list width, height, fps and mat format

--- tools/lsvideo/lsvideo.cpp
+++ tools/lsvideo/lsvideo.cpp
@@ -1,11 +1,24 @@
 #include <iostream>
 #include <indrome/video.h>
+#include <opencv2/videoio.hpp>
 
 int main() 
 {
 
     std::cout << "Listing found camera ids:" << std::endl; 
     for(auto id: indrome::get_video_device_id())
-        std::cout << "id: " << id << std::endl;
+    {
+        auto cap = cv::VideoCapture(id);
+        auto width = cap.get(cv::CAP_PROP_FRAME_WIDTH);
+        auto height = cap.get(cv::CAP_PROP_FRAME_HEIGHT);
+        auto fps = cap.get(cv::CAP_PROP_FPS);
+        auto format = cap.get(cv::CAP_PROP_FORMAT);
+        std::cout << "* id: " << id;
+        if(width != -1 && height != -1) std::cout << " size: " << width 
+            << "x" << height 
+            << " fps: " << fps
+            << " format: " << format
+            << std::endl;
+    }
     return 0;
 }
