5b3428871ad3f1c1daded19d04f0220318c872f9
Tobias Ulvgard <tobias.ulvgard@dirac.se>
2016-09-25 17:11:30 +0200

    Add tool resizevid

---
+++ tools/resizevid/resizevid.cpp
@@ -0,0 +1,29 @@
+#include <opencv2/opencv.hpp>
+#include <iostream>
+#include <indrome/util.h>
+
+void usage() 
+{
+    std::cout << "Usage: WIDTH HEIGHT" << std::endl;
+    std::cout << "Resize frame from stdin to WIDTHxHEIGHT and output on stdout" << std::endl;
+}
+
+int main(int argc, char* argv[]) 
+{
+
+    if(argc != 3)
+    {
+        usage();
+        return -1;
+    }
+
+    auto width = atoi(argv[1]);
+    auto height = atoi(argv[2]);
+
+    for(;;)
+    {
+        cv::Mat frame = indrome::read_frame_from_stdin();
+        cv::resize(frame, frame, cv::Size(width,height));
+        indrome::write_frame_to_stdout(frame);
+    }
+}
