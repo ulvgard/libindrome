e5330f1d6bc9f9926961c7a9affc1e58da2c312b
Tobias Ulvgard <tobias.ulvgard@gmail.com>
2016-10-22 14:54:35 +0200

    Add stop detection to showvid

--- tools/showvid/showvid.cpp
+++ tools/showvid/showvid.cpp
@@ -1,14 +1,25 @@
 #include <indrome/util.h>
 #include <opencv2/opencv.hpp>
 #include <opencv2/highgui.hpp>
+#include <iostream>
+#include <string>
 
-int main()
+int main(int argc, char* argv[])
 {
     for(;;) {
-        cv::Mat frame = indrome::read_frame_from_stdin();
-        cv::imshow("", frame);
+
+        cv::Mat view = indrome::read_frame_from_stdin();
+
+        if(view.empty())
+        {
+            std::cerr << argv[0] << ": received close signal" << std::endl;
+            indrome::write_close_signal_to_stdout();
+            break;
+        }
+
+        cv::imshow(window_name, view);
         cv::waitKey(5);
-        indrome::write_frame_to_stdout(frame);
+        indrome::write_frame_to_stdout(view);
     }
 
     return 0;
